0fa47c458165397624e36863b719c602a98aaefa
EndaMolloy <enda.molloy.1@gmail.com>
2017-11-01 18:13:10 +0000

    added session completion list

--- pomodoro.js
+++ pomodoro.js
@@ -75,6 +75,7 @@ window.onload = function() {
     if(!seconds){
       alert("Times Up")
       window.clearInterval(intervalID)
+      addToCompletedSession()
     }
 
   }
@@ -113,7 +114,7 @@ window.onload = function() {
      //also one for mouse release
 
      var start = moment();
-     var end = moment(start).add(userInput*60,'seconds');
+     var end = moment(start).add(userInput*60,'seconds'); /////////////should be (userInput*60,'seconds')
      intervalID = window.setInterval(checkTime, 1000, end);
 
      console.log("start time: "+start.format("LTS"));
@@ -121,5 +122,130 @@ window.onload = function() {
 
    })
 
+   //make a copy of the input div
+   var originalState = $("#inputContainer").clone();
+   var completedSessionsArr = [];
+
+   $(document).on('keypress', '#taskInput', function(e) {
+     if (e.which == 13 && $(this).val().length > 0) {
+       addButtons(this.value)
+     }
+    })
+
+
+
+   function addButtons(task){
+
+     $("#inputContainer").replaceWith('<div id=taskConfirmed></div>')
+
+     var taskDisplay = document.getElementById('taskConfirmed');
+     taskDisplay.innerHTML = '';
+
+     var taskLabel = document.createElement('label');
+     var delBtn = document.createElement('span');
+     var checkbox = document.createElement('input');
+
+     taskLabel.className = 'taskLabel';
+     taskLabel.textContent = task;
+
+     delBtn.className = 'deleteTaskBtn';
+		 delBtn.textContent = 'x';
+     delBtn.id = 'delete'
+		 //delBtn.onclick = deleteTask(originalState);
+
+     checkbox.className = 'taskCheckbox';
+     checkbox.id = 'checkbox'
+     checkbox.type = 'checkbox';
+		 checkbox.checked = task.done;
+		 //checkbox.onclick = toggleChecked;
+
+     taskDisplay.appendChild(checkbox);
+     taskDisplay.appendChild(taskLabel);
+     taskDisplay.appendChild(delBtn);
+
+   }
+
+   //creating elements dynamically, you need to use event delegation using 'on'
+   //Delegated event by attaching the event to the document or to the parent element that exists at any point in time.
+   $(document).on('click','#delete',function(){
+     $("#taskConfirmed").replaceWith(originalState)
+   })
+
+   $(document).on('click','#checkbox', function(){
+    if (this.checked) {
+        $('.taskLabel').css('text-decoration', 'line-through')
+        //addToCompletedTask(completedTask)
+    }
+})
+
+function addToCompletedSession(){
+
+  var completedTask = $('.taskLabel').text();
+  console.log('completedTask',completedTask);
+
+
+  var id = (completedSessionsArr.length + 1).toString();
+
+  completedSessionsArr.push({
+    'id' : id,
+    'task': completedTask,
+    'time': sessionValue,
+    'timestamp': moment().format()
+  })
+
+  //console.log(completedSessionsArr);
+  if(completedSessionsArr.length===6){
+    alert('Please commit before starting next session')
+  }
+  else{
+    updateSessionList()
+  }
+}
+
+function updateSessionList(){
+  var ul = document.getElementById('myCompletedTask');
+  ul.innerHTML = '';
+
+  completedSessionsArr.forEach(task=>{
+
+        // console.log('task',task.task);
+        // console.log('taskID', task.id);
+        // console.log('tasktime', task.time);
+
+    var listitem = document.createElement('li'),
+        id = document.createElement('label'),
+        taskLabel = document.createElement('label'),
+        timeLabel = document.createElement('label'),
+        delSessBtn = document.createElement('span')
+
+        id.textContent = task.id;
+        taskLabel.textContent = task.task;
+        timeLabel.textContent = task.time;
+        delSessBtn.textContent = 'x';
+
+        listitem.appendChild(id);
+        listitem.appendChild(taskLabel)
+        listitem.appendChild(timeLabel)
+        listitem.appendChild(delSessBtn)
+
+        ul.appendChild(listitem)
+
+  })
+
+
+
+
+}
+
+$("#deleteArr").click(function(){
+
+    completedSessionsArr = [];
+    console.log(completedSessionsArr);
+})
+
+$("#view").click(function(){
+
+    console.log(completedSessionsArr);
+})
 
 }
